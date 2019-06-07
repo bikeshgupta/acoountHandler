d7ea12933f45d982e455bc9bbb8327d12a79e782
bikegupt <bikegupt@BIKEGUPT-2VKS1.partnet.cisco.com>
2019-06-07 12:33:04 +0530

    schedular

--- user-accounts/src/main/java/com/moonsea/useraccounts/repos/UserBeanRepo.java
+++ user-accounts/src/main/java/com/moonsea/useraccounts/repos/UserBeanRepo.java
@@ -1,10 +1,16 @@
 package com.moonsea.useraccounts.repos;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.moonsea.useraccounts.bean.UserBean;
 
 @Repository
 public interface UserBeanRepo extends JpaRepository<UserBean, Long> {
+
+	List<UserBean> findByUsernameIn(List<String> users);
 }
